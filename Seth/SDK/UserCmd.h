f0c735918e32d3d8581eea432d58f339e4ae5b97
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2023-02-20 02:43:46 -0300

    Update usercmd actions

--- Seth/SDK/UserCmd.h
+++ Seth/SDK/UserCmd.h
@@ -10,14 +10,28 @@ struct UserCmd {
         IN_FORWARD = 1 << 3,
         IN_BACK = 1 << 4,
         IN_USE = 1 << 5,
+        IN_CANCEL = 1 << 6,
+        IN_LEFT = 1 << 7,
+        IN_RIGHT = 1 << 8,
         IN_MOVELEFT = 1 << 9,
         IN_MOVERIGHT = 1 << 10,
         IN_ATTACK2 = 1 << 11,
+        IN_RUN = 1 << 12,
+        IN_RELOAD = 1 << 13,
+        IN_ALT1 = 1 << 14,
+        IN_ALT2 = 1 << 15,
         IN_SCORE = 1 << 16,
         IN_SPEED = 1 << 17,
+        IN_WALK = 1 << 18,
         IN_ZOOM = 1 << 19,
-        IN_BULLRUSH = 1 << 22
+        IN_WEAPON1 = 1 << 20,
+        IN_WEAPON2 = 1 << 21,
+        IN_BULLRUSH = 1 << 22,
+        IN_GRENADE1 = 1 << 23,
+        IN_GRENADE2 = 1 << 24,
+        IN_ATTACK3 = 1 << 25,
     };
+
     void* vmt;
     int commandNumber;
     int tickCount;
